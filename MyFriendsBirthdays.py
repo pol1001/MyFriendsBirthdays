99734cb039fc6292735987746445886cc18ed8cb
Pol <spbgal@yandex.ru>
2021-05-19 22:31:11 +0300

    The main functionality of the program is written

--- MyFriendsBirthdays.py
+++ MyFriendsBirthdays.py
@@ -3,19 +3,30 @@ import os
 import sys
 import datetime
 
+def month_from_num(number):
+    month_dict = {1:'январь', 2:'февраль', 3:'март', 4:'апрель',5:'май',
+                  6:'июнь',7:'июль', 8:'август',9:'сентябрь', 10:'октябрь',
+                  11:'ноябрь', 12:'декабрь'
+    }
+    return month_dict.get(number, 'Неизвестный месяц')
 
 def load_data_from_file(file, arr_list):
     if not (os.path.isfile(file)):
         print('Файл: {} не найден!'.format(file))
-        return None
+
     userarrdata = []
     infile = open(file, 'r', encoding='utf-8')
     for line in infile:
         temp = line.strip()
-        if temp.startswith('#'):
+        if temp.startswith('#') or temp == '':
             continue
         else:
-            arr = list(map(lambda x: x.strip(), temp.split('\t')))
+            arr = list(map(str.strip, temp.split('\t')))
+            if len(arr) != 3:
+                print(arr)
+                print('Данные повреждены!\n'
+                      'Записи должны разделяться символом табуляции!')
+                sys.exit()
             userarrdata.append(arr[:])
     infile.close()
     my_dict = {}
@@ -25,6 +36,7 @@ def load_data_from_file(file, arr_list):
         my_dict['day'] = int(i[1][0:2])
         my_dict['month'] = int(i[1][3:5])
         my_dict['year'] = int(i[1][-4:])
+        my_dict['birthday'] = i[1]
         arr_list.append(my_dict.copy())
         my_dict.clear()
 
@@ -47,12 +59,13 @@ def get_prev_month(current_month):
     return 12
 
 
-def print_birthdays(current_day, current_month, current_year, user_arr):
+def select_case_birthdays(current_day, current_month, current_year, user_arr):
     prev_month = get_prev_month(current_month)
     next_month = get_next_month(current_month)
     prev_month_data = []
     next_month_data = []
     current_month_data = []
+
     for i in range(len(user_arr)):
         if user_arr[i]['month'] == prev_month:
             prev_month_data.append(user_arr[i])
@@ -60,24 +73,65 @@ def print_birthdays(current_day, current_month, current_year, user_arr):
             next_month_data.append(user_arr[i])
         elif user_arr[i]['month'] == current_month:
             current_month_data.append(user_arr[i])
-    for i in range(len(prev_month_data)):
-        print('Ранее: nickname: {}, имя: {}, возраст {} '.format(
-            prev_month_data[i]['nickname'],
-            prev_month_data[i]['name'],
-            current_year - prev_month_data[i]['year']
-            ))
+
+    print_birthdays_month(current_day, current_month, current_year,
+                               prev_month_data, 'prev')
+
+    print_birthdays_month(current_day, current_month, current_year,
+                                  current_month_data, 'current')
+
+    print_birthdays_month(current_day, current_month, current_year,
+                               next_month_data, 'next')
+
+
 
 #
-#
+def print_birthdays_month(current_day, current_month, current_year,
+                               user_arr, flag):
+    print()
+    for i in range(len(user_arr)):
+        if flag == 'prev':
+            word = 'Ранее'
+            age = current_year - user_arr[i]['year']
+            if current_month == 1:
+                age -= 1
+        elif flag == 'next':
+            word = 'Далее'
+            age = current_year - user_arr[i]['year'] - 1
+            if current_month == 12:
+                age += 1
+        elif flag == 'current':
+            if user_arr[i]['day'] < current_day:
+                age = current_year - user_arr[i]['year']
+                word = 'Чуть ранее'
+            elif user_arr[i]['day'] == current_day:
+                age = current_year - user_arr[i]['year']
+                word = 'Сегодня!'
+            elif user_arr[i]['day'] > current_day:
+                age = current_year - user_arr[i]['year'] - 1
+                word = 'Совсем скоро'
+            else:
+                word = 'Сейчас'
+                age = 0
+        else:
+            word = ''
+            age = 0
+        # print(f'{word}: nickname: {user_arr[i]["nickname"]}, имя: {user_arr[i]["name"]}, возраст: {age}, дата рождения: {user_arr[i]["birthday"]}')
+        nick = user_arr[i]["nickname"] + ', '
+        if nick == '-, ':
+            nick = ''
+        print(f'{word}: {nick}{user_arr[i]["name"]}, {age}, '
+              f'{user_arr[i]["birthday"]}')
+
 
 
 if sys.platform[0:6] != 'linux':
     print('This program for Linux only!')
-    exit()
+    sys.exit()
 
 if sys.version_info[0] < 3:
     print('This program for Python version >= 3 !')
-    exit()
+    sys.exit()
 
 userHome = os.path.expanduser('~')
 userName = os.path.split(userHome)[-1]
@@ -93,12 +147,14 @@ else:
     print('Данные не загружены! Программа завершает работу')
     sys.exit()
 
-nowData = datetime.datetime.now()
-nowDateDay, nowDateMonth= nowData.day, nowData.month
-nowDateYear = nowData.year
+nowDate = datetime.datetime.now()
+nowDateDay, nowDateMonth = nowDate.day, nowDate.month
+nowDateYear = nowDate.year
+#  #nowDateDay, nowDateMonth, nowDateYear = 2,1,2022
+print(f'Сегодня: {nowDateDay} {month_from_num(nowDateMonth)} '
+      f'{nowDateYear} год')
 
-print_birthdays(nowDateDay, nowDateMonth, nowDateYear, userArrDictData)
+select_case_birthdays(nowDateDay, nowDateMonth, nowDateYear, userArrDictData)
 
 
 
-#print(userArrDictData)
